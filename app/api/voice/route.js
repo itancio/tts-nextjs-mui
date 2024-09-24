fbf64e363d5a83997cfa277e4bf89e922ee560e2
Irvin Tancioco <irvin.tanc@gmail.com>
2024-09-23 23:50:37 -0700

    implement writeAudioFile

--- app/api/voice/route.js
+++ app/api/voice/route.js
@@ -16,43 +16,48 @@ export async function POST(req){
     console.log("Initializing request: ", text, voice)
 
     try {
-        const filePath = await getAudio(text, voice)
-        return NextResponse.json({ audioUrl: filePath });
+      const filePath = await getAudio(text, voice)
+      return NextResponse.json({ audioUrl: filePath });
     } catch (error) {
-        console.error("Error during API request:", error.message);
-        console.error("Stack trace:", error.stack);
-        return NextResponse.json({ error: 'Error generating audio' }, { status: response.result.status });
+      console.error("Error during API request:", error.message);
+      console.error("Stack trace:", error.stack);
+      return NextResponse.json({ error: 'Error generating audio' }, { status: response.result.status });
     }
 }
 
-const getAudio = async (text, voice) => {
+const getAudio = async (text, model) => {
+  console.log('getaudio text: ', text, ' model: ', model)
   // STEP 2: Make a request and configure the request with options (such as model choice, audio configuration, etc.)
-  const response = await deepgram.speak.request({ text }, {voice});
+  const response = await deepgram.speak.request({text: text}, {model: 'aura-asteria-en'});
 
-  console.log('Getting response from deepgram: ', response.result.ok)
+  console.log('deepgram: ', deepgram)
+  console.log('Getting response from deepgram: ', response)
 
   // STEP 3: Get the audio stream and headers from the response
   if (response.result.ok) {
     const stream = await response.getStream();
+    console.log('Getting incoming stream: ', stream)
     const headers = await response.getHeaders();
-    console.log('Getting stream', stream)
+
+    if (headers) {
+      console.log('Headers: ', headers)
+    }
+
+    if (stream) {
+      // STEP 4: Convert the stream to an audio buffers
+      const buffer = await getAudioBuffer(stream);
+  
+      console.log('Getting buffer: ', buffer)
+  
+      // STEP 5: Write the audio buffer to a file
+      const filename = await writeAudioFile(buffer)
+      return NextResponse.json({audioUrl: filename})
+    } else {
+      console.error('Error:', response.result.statusText);
+      return NextResponse.json({ error: 'Error generating audio' }, { status: response.result.status });
+    }
   } else {
     console.log('No response from deepgram: ', response.result)
-  }
-
-
-  if (stream) {
-    // STEP 4: Convert the stream to an audio buffers
-    const buffer = await getAudioBuffer(stream);
-
-    console.log('Getting buffer: ', buffer)
-
-    // STEP 5: Write the audio buffer to a file
-    writeAudioFile(buffer)
-    return "/audio/audio.wav"
-  } else {
-    console.log("Error generating audio:", stream)
-    throw new Error("Error generating audio: Stream is empty")
   }
 }
 
@@ -60,22 +65,32 @@ const getAudio = async (text, voice) => {
 const writeAudioFile = async(buffer) => {
   try {
     // Create 'audio' directory if it doesn't exist
-    const audioDirectory = path.join(__dirname, "audio");
+    const audioDirectory = path.join(process.cwd(), 'public', "audio");
+
     if (!fs.existsSync(audioDirectory)) {
         fs.mkdirSync(audioDirectory);
     }
 
     // Write audio file to 'audio' directory
-    await new Promise( (resolve, reject) => {
-        fs.writeFile(path.join(audioDirectory, 'audio.wav'), buffer, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                console.log("Audio file saved successfully.");
-                resolve();
-            }
-        })
-    })
+    const filename = `audio_${Date.now()}.wav`
+    const filePath = path.join(audioDirectory, filename);
+
+    await fs.promises.writeFile(filePath, buffer)
+
+    console.log("Audio file saved successfully at: ${filePath}")
+
+    return `/audio/${filename}`
+
+    // await new Promise( (resolve, reject) => {
+    //     fs.writeFile(filePath, buffer, (err) => {
+    //         if (err) {
+    //             reject(err);
+    //         } else {
+    //             console.log("Audio file saved successfully.");
+    //             resolve();
+    //         }
+    //     })
+    // })
 
   } catch (error) {
     console.error("Error writing audio file:", error.message);
